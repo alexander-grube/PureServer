b351f64f38a3baf0c8df311fccd6fa1f77ed869e
Alexander Grube <alexp.grube@googlemail.com>
2023-11-25 10:46:47 +0100

    use gradle 8.5 rc4 and update dep

--- build.gradle.kts
+++ build.gradle.kts
@@ -15,13 +15,13 @@ repositories {
 }
 
 dependencies {
-    implementation("io.undertow:undertow-core:2.3.7.Final")
-    implementation("io.undertow:undertow-servlet:2.3.7.Final")
-    implementation("io.undertow:undertow-websockets-jsr:2.3.7.Final")
-    implementation("com.alibaba.fastjson2:fastjson2:2.0.41")
+    implementation("io.undertow:undertow-core:2.3.10.Final")
+    implementation("io.undertow:undertow-servlet:2.3.10.Final")
+    implementation("io.undertow:undertow-websockets-jsr:2.3.10.Final")
+    implementation("com.alibaba.fastjson2:fastjson2:2.0.42")
     implementation("org.postgresql:postgresql:42.6.0")
     implementation("org.furyio:fury-core:0.3.0")
-    implementation("com.zaxxer:HikariCP:5.0.1")
+    implementation("com.zaxxer:HikariCP:5.1.0")
     implementation("io.github.cdimascio:java-dotenv:5.2.2")
     testImplementation(platform("org.junit:junit-bom:5.9.1"))
     testImplementation("org.junit.jupiter:junit-jupiter")
