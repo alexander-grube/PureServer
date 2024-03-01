2d3fe5e69033df3aff0a6d1c555461eea11a0841
Alexander Grube <alexp.grube@googlemail.com>
2024-03-01 22:37:12 +0100

    use gradle 8.7 rc2 and update dep

--- build.gradle.kts
+++ build.gradle.kts
@@ -15,17 +15,17 @@ repositories {
 }
 
 dependencies {
-    implementation("io.undertow:undertow-core:2.3.10.Final")
-    implementation("io.undertow:undertow-servlet:2.3.10.Final")
-    implementation("io.undertow:undertow-websockets-jsr:2.3.10.Final")
-    implementation("com.alibaba.fastjson2:fastjson2:2.0.46")
-    implementation("org.postgresql:postgresql:42.7.1")
+    implementation("io.undertow:undertow-core:2.3.12.Final")
+    implementation("io.undertow:undertow-servlet:2.3.12.Final")
+    implementation("io.undertow:undertow-websockets-jsr:2.3.12.Final")
+    implementation("com.alibaba.fastjson2:fastjson2:2.0.47")
+    implementation("org.postgresql:postgresql:42.7.2")
     implementation("org.furyio:fury-core:0.4.1")
     implementation("com.zaxxer:HikariCP:5.1.0")
     implementation("io.github.cdimascio:java-dotenv:5.2.2")
-    testImplementation(platform("org.junit:junit-bom:5.10.1"))
-    testImplementation("org.junit.jupiter:junit-jupiter:5.10.1")
-    testImplementation("org.testcontainers:testcontainers:1.19.4")
+    testImplementation(platform("org.junit:junit-bom:5.10.2"))
+    testImplementation("org.junit.jupiter:junit-jupiter:5.10.2")
+    testImplementation("org.testcontainers:testcontainers:1.19.6")
 }
 
 tasks.test {
